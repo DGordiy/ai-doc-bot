39d1347f8abd5316ed7f7dcf63a46dc17da2a788
dgordiy <29153504+DGordiy@users.noreply.github.com>
2025-11-16 19:53:25 +0100

    enhance TxtFileProcessor with encoding

--- backend/services/file_processors/txt_file_processor.py
+++ backend/services/file_processors/txt_file_processor.py
@@ -2,6 +2,6 @@ from backend.services.file_processors.file_processor import FileProcessor
 
 
 class TxtFileProcessor(FileProcessor):
-    def process_file(self, file_path: str) -> str:
+    def process_file(self, file_path: str, encoding='utf-8') -> str:
         with open(file_path, 'r') as f:
             return f.read()
